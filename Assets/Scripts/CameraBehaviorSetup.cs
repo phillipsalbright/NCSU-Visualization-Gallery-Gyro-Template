9461895f7bdc229656b816ce35ee99e32b06537d
Phillips Albright <55517109+phillipsalbright@users.noreply.github.com>
2023-04-14 16:29:07 -0400

    CameraBehaviorSetup code for emulation of screen resolution on other monitors

--- Assets/Scripts/CameraBehaviorSetup.cs
+++ Assets/Scripts/CameraBehaviorSetup.cs
@@ -8,6 +8,13 @@ public class CameraBehaviorSetup : MonoBehaviour
     void Start()
     {
         Screen.SetResolution(15360, 1080, true);
+        //if you want the windows build to emulate the vis gallery on any monitor, uncomment this code and delete line 10:
+        /**
+        int resx = Screen.resolutions[0].width;
+        int resy = Screen.resolutions[0].height;
+        Screen.SetResolution(resx, Mathf.RoundToInt(1080 * resx / 15360), true);
+        */
+
     }
 
     // Update is called once per frame
